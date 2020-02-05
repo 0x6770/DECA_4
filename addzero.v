ca4bd8347fac9ca148ee2bfd219b61fc21912b3f
Yujie Wang <yujie@charleswang.name>
2020-02-05 11:44:10 +0000

    change addzero component

--- addzero.v
+++ addzero.v
@@ -1,12 +1,14 @@
 module addzero(
 input [15:0] ramout,
+input useAllBits,
 output [15:0] z
 );
 
-assign z[11:0] =  ramout[11:0];
-assign z[15] = 0;
-assign z[14] = 0;
-assign z[13] = 0;
-assign z[12] = 0;
+assign zero = 0;
+assign z[11:0] = useAllBits ? ramout : {zero,zero,zero,zero,ramout[11:0]};
+//assign z[15] = 0;
+//assign z[14] = 0;
+//assign z[13] = 0;
+//assign z[12] = 0;
 
 endmodule
