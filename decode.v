4815ec3f1b93f7672745aca94098f67dc517372a
Yujie Wang <yujie@charleswang.name>
2020-02-07 23:01:41 +0000

    modified decode.v

--- decode.v
+++ decode.v
@@ -23,13 +23,14 @@ assign JMI = !IR[3] & IR[2] & !IR[1] & IR[0];
 assign JEQ = !IR[3] & IR[2] & IR[1] & !IR[0];
 assign STP = !IR[3] & IR[2] & IR[1] & IR[0];
 assign LDI = IR[3] & !IR[2] & !IR[1] & !IR[0];
+assign LDX = IR[3] & !IR[2] & !IR[1] & IR[0];
 
 assign EXTRA = LDA & EXEC1 | ADD & EXEC1 | SUB & EXEC1;
 assign Wren = STA & EXEC1;
 assign MUX1 = LDA & EXEC1 | STA & EXEC1 | ADD & EXEC1 | SUB & EXEC1;
 assign MUX3 = LDA & EXEC2 | LDI & EXEC1;
 assign PC_sload = JMP & EXEC1 | JMI & EXEC1 & MI | JEQ & EXEC1 & EQ;
-assign PC_cnt_en = LDA & EXEC2 | STA & EXEC1 | ADD & EXEC2 | SUB & EXEC2 | JMI & EXEC1 & !MI | JEQ & EXEC1 & EQ | LDI & EXEC1;
+assign PC_cnt_en = LDA & EXEC2 | STA & EXEC1 | ADD & EXEC2 | SUB & EXEC2 | JMI & EXEC1 & !MI | JEQ & EXEC1 & !EQ | LDI & EXEC1;
 assign ACC_EN = LDA & EXEC2 | ADD & EXEC2 | SUB & EXEC2 | LDI & EXEC1;
 assign ACC_LOAD = LDA & EXEC2 | ADD & EXEC2 | SUB & EXEC2 | LDI & EXEC1;
 assign ADDSUB = ADD & EXEC2;
