4148dfb4581877a6230fc6c353c932d73718c276
Yujie Wang <yujie@charleswang.name>
2020-02-17 14:21:41 +0000

    add ASR and LSR

--- decode.v
+++ decode.v
@@ -25,18 +25,21 @@ module decode(input FETCH,
     assign JEQ = !IR[3] & IR[2] & IR[1] & !IR[0];
     assign STP = !IR[3] & IR[2] & IR[1] & IR[0];
     assign LDI = IR[3] & !IR[2] & !IR[1] & !IR[0];
-    assign LDX = IR[3] & !IR[2] & !IR[1] & IR[0];
+    assign LSR = IR[3] & !IR[2] & IR[1] & !IR[0];
+    assign ASR = IR[3] & !IR[2] & IR[1] & IR[0];
     
     assign EXTRA           = LDA & EXEC1 | ADD & EXEC1 | SUB & EXEC1;
     assign Wren            = STA & EXEC1;
     assign MUX1            = LDA & EXEC1 | STA & EXEC1 | ADD & EXEC1 | SUB & EXEC1;
     assign MUX3            = LDA & EXEC2 | LDI & EXEC1;
     assign PC_sload        = JMP & EXEC1 | JMI & EXEC1 & MI | JEQ & EXEC1 & EQ;
-    assign PC_cnt_en       = LDA & EXEC2 | STA & EXEC1 | ADD & EXEC2 | SUB & EXEC2 | JMI & EXEC1 & !MI | JEQ & EXEC1 & !EQ | LDI & EXEC1;
-    assign ACC_EN          = LDA & EXEC2 | ADD & EXEC2 | SUB & EXEC2 | LDI & EXEC1;
+    assign PC_cnt_en       = LDA & EXEC2 | STA & EXEC1 | ADD & EXEC2 | SUB & EXEC2 | JMI & EXEC1 & !MI | JEQ & EXEC1 & !EQ | LDI & EXEC1 | LSR & EXEC1 | ASR & EXEC1;
+    assign ACC_EN          = LDA & EXEC2 | ADD & EXEC2 | SUB & EXEC2 | LDI & EXEC1 | LSR & EXEC1 | ASR & EXEC1;
     assign ACC_LOAD        = LDA & EXEC2 | ADD & EXEC2 | SUB & EXEC2 | LDI & EXEC1;
     assign ADDSUB          = ADD & EXEC2;
-    assign ACC_SHIFTIN     = 0;
-    assign MUX3_useAllBits = LDA & EXEC2;
+    // assign ACC_SHIFTIN     = LSR & EXEC1;
+    assign ACC_SHIFTIN     = ASR & EXEC1 & MI;
+    // assign ACC_SHIFTIN     = 0;
+    assign MUX3_useAllBits = LDA & EXEC2 | LDA & EXEC2 | LSR & EXEC1 | ASR & EXEC1;
     
 endmodule
