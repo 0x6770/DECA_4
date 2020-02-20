96ab11340862a089337907a3d8295225c42580c1
Yujie Wang <yujie@charleswang.name>
2020-02-20 04:32:03 +0000

    pipeline kind of work, but still have errors

--- decode.v
+++ decode.v
@@ -4,6 +4,7 @@ module decode(input FETCH,
               input EQ,
               input MI,
               input [3:0] IR,
+              input clk,
               output EXTRA,
               output Wren,
               output MUX1,
@@ -14,32 +15,58 @@ module decode(input FETCH,
               output ACC_LOAD,
               output ACC_SHIFTIN,
               output ADDSUB,
-              output MUX3_useAllBits);
+              output MUX3_useAllBits,
+              output P,
+              output afterE2,
+              output TF);
     
-    assign LDA = !IR[3] & !IR[2] & !IR[1] & !IR[0];
-    assign STA = !IR[3] & !IR[2] & !IR[1] & IR[0];
-    assign ADD = !IR[3] & !IR[2] & IR[1] & !IR[0];
-    assign SUB = !IR[3] & !IR[2] & IR[1] & IR[0];
-    assign JMP = !IR[3] & IR[2] & !IR[1] & !IR[0];
-    assign JMI = !IR[3] & IR[2] & !IR[1] & IR[0];
-    assign JEQ = !IR[3] & IR[2] & IR[1] & !IR[0];
-    assign STP = !IR[3] & IR[2] & IR[1] & IR[0];
-    assign LDI = IR[3] & !IR[2] & !IR[1] & !IR[0];
-    assign LSR = IR[3] & !IR[2] & IR[1] & !IR[0];
-    assign ASR = IR[3] & !IR[2] & IR[1] & IR[0];
+    assign P = LDI | LDA | ADD | SUB | LSR;
+    
+    assign JMP = !IR[3] & IR[2] & !IR[1] & !IR[0];      //* 2
+    assign JEQ = !IR[3] & IR[2] & IR[1] & !IR[0];       //* 2
+    assign JMI = !IR[3] & IR[2] & !IR[1] & IR[0];       //* 2
+    assign LDA = !IR[3] & !IR[2] & !IR[1] & !IR[0];     //* 3
+    assign LDI = IR[3] & !IR[2] & !IR[1] & !IR[0];      //! 2
+    assign STA = !IR[3] & !IR[2] & !IR[1] & IR[0];      //* 2
+    assign ADD = !IR[3] & !IR[2] & IR[1] & !IR[0];      //* 3
+    assign SUB = !IR[3] & !IR[2] & IR[1] & IR[0];       //* 3
+    assign STP = !IR[3] & IR[2] & IR[1] & IR[0];        //* 2
+    assign LSR = IR[3] & !IR[2] & IR[1] & !IR[0];       //! 2
+    assign ASR = IR[3] & !IR[2] & IR[1] & IR[0];        //! 2
+    
+    // instantiate 1-bit DFF for pipeline state
+    RisingEdge_DFF pipelineState(
+    .D (EXEC2),
+    .clk (clk),
+    .Q (afterE2)
+    );
+    
+    assign TF = afterE2 & EXEC1 & (LDI | STA | JMP | JMI | JEQ);
     
     assign EXTRA           = LDA & EXEC1 | ADD & EXEC1 | SUB & EXEC1;
     assign Wren            = STA & EXEC1;
     assign MUX1            = LDA & EXEC1 | STA & EXEC1 | ADD & EXEC1 | SUB & EXEC1;
     assign MUX3            = LDA & EXEC2 | LDI & EXEC1;
     assign PC_sload        = JMP & EXEC1 | JMI & EXEC1 & MI | JEQ & EXEC1 & EQ;
-    assign PC_cnt_en       = LDA & EXEC2 | STA & EXEC1 | ADD & EXEC2 | SUB & EXEC2 | JMI & EXEC1 & !MI | JEQ & EXEC1 & !EQ | LDI & EXEC1 | LSR & EXEC1 | ASR & EXEC1;
+    // assign PC_cnt_en    = LDA & EXEC2 | STA & EXEC1 | ADD & EXEC2 | SUB & EXEC2 | JMI & EXEC1 & !MI | JEQ & EXEC1 & !EQ | LDI & EXEC1 | LSR & EXEC1 | ASR & EXEC1;
+    assign PC_cnt_en       = FETCH | EXEC1 & (LDA | ADD | SUB) | afterE2 & EXEC1 & (LDI | STA);
     assign ACC_EN          = LDA & EXEC2 | ADD & EXEC2 | SUB & EXEC2 | LDI & EXEC1 | LSR & EXEC1 | ASR & EXEC1;
     assign ACC_LOAD        = LDA & EXEC2 | ADD & EXEC2 | SUB & EXEC2 | LDI & EXEC1;
     assign ADDSUB          = ADD & EXEC2;
-    // assign ACC_SHIFTIN     = LSR & EXEC1;
+    // assign ACC_SHIFTIN  = LSR & EXEC1;
     assign ACC_SHIFTIN     = ASR & EXEC1 & MI;
-    // assign ACC_SHIFTIN     = 0;
+    // assign ACC_SHIFTIN  = 0;
     assign MUX3_useAllBits = LDA & EXEC2 | LDA & EXEC2 | LSR & EXEC1 | ASR & EXEC1;
     
 endmodule
+    
+    // Verilog code for D Flip FLop
+    module RisingEdge_DFF(D,clk,Q);
+        input D; // Data input
+        input clk; // clock input
+        output reg Q; // output Q
+        always @(posedge clk)
+        begin
+            Q <= D;
+        end
+    endmodule
--- state_machine.v
+++ state_machine.v
@@ -1,17 +1,17 @@
-module state_machine(
-	input EXTRA,
-	input [2:0] s,
-	output FETCH,
-	output EXEC1,
-	output EXEC2,
-	output [2:0] N
-);
-
-assign N[2] = 0;
-assign N[1] = !s[2] & !s[1] & s[0] & EXTRA;
-assign N[0] = !s[2] & !s[1] & !s[0];
-assign FETCH = !s[2] & !s[1] & !s[0];
-assign EXEC1 = !s[2] & !s[1] & s[0];
-assign EXEC2 = !s[2] & s[1] & !s[0];
-
+module state_machine(input EXTRA,
+                     input P,
+                     input TF,
+                     output FETCH,
+                     output EXEC1,
+                     output EXEC2,
+                     output [2:0] N,
+                     input [2:0] s);
+    
+    assign N[2]  = 0;
+    assign N[1]  = !s[2] & !s[1] & s[0] & EXTRA;
+    assign N[0]  = (!s[2] & !s[1] & !s[0]) | (!s[2] & !s[1] & s[0] & !EXTRA & P & !TF) | (!s[2] & s[1] & !s[0] & P);
+    assign FETCH = !s[2] & !s[1] & !s[0];
+    assign EXEC1 = !s[2] & !s[1] & s[0];
+    assign EXEC2 = !s[2] &  s[1] & !s[0];
+    
 endmodule
